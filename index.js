5db333dfe2ac8288aed0db61a33f716b786804b1
Guillermo Cardoze <vital_tmv@yahoo.com>
2023-10-10 17:01:55 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+
+main.remove();
+
+
+const newHeader = document.querySelector("h1");
+
+const id = document.getElementById('victory');
+
+victory.textContent = "Guillermo is the champion";
+
+
+
+
+
+
+
+
+
+
